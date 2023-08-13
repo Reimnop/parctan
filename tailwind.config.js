aaf63157c54f1d859f52de6665a126ce5dddc227
enchart <77974051+enchart@users.noreply.github.com>
2023-08-13 23:15:13 +0500

    Added themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,13 @@
+const theme = {
+  primary: "#ff5eda",
+  secondary: "#292929",
+  accent: "#31b8ec",
+  info: "#38bdf8",
+  success: "#4ade80",
+  warning: "#facc15",
+  error: "#fb7185",
+};
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
@@ -5,4 +15,20 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...theme,
+          neutral: "#d6d3d1",
+          "base-100": "#f5f5f4",
+        },
+        dark: {
+          ...theme,
+          neutral: "#292524",
+          "base-100": "#1c1917",
+        },
+      },
+    ],
+  },
 };
