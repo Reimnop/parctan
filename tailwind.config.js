4fb26d4bd524a6844de9a6a7ca92751349ce43fb
enchart <77974051+enchart@users.noreply.github.com>
2023-08-16 22:11:25 +0500

    Added basic animations (TBD)

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,39 @@ const theme = {
   error: "#fb7185",
 };
 
+function generateAnimations(animations) {
+  const animation = {};
+  const keyframes = {};
+  for (const [anim, kfs] of Object.entries(animations)) {
+    for (const [animName, values] of Object.entries(kfs)) {
+      animation[animName] = `${anim} ${animName}`;
+      keyframes[animName] = values;
+    }
+  }
+  return { animation, keyframes };
+}
+
+const animations = generateAnimations({
+  "1s cubic-bezier(0.85, 0, 0.15, 1) 0s infinite alternate both running": {
+    "root-1-editor": {
+      "0%": { opacity: "50%" },
+      "100%": { transform: "scale(1.05) rotate(2deg) translate(1em, -1.1em)" },
+    },
+  },
+});
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        ...animations.animation
+      },
+      keyframes: {
+        ...animations.keyframes
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
