4fead0524df758de7043a5493a2b5d20760b841b
enchart <77974051+enchart@users.noreply.github.com>
2023-08-17 22:10:54 +0500

    Added meta and icon

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,6 @@
 export const load = () => {
   return {
-    title: "Parctan",
+    title: "Parctan — the advanced Project Arrhythmia experience",
     description:
       "The advanced Project Arrhythmia experience: tools, mods and everything else collected in one place.",
     url: "https://parctan.com/",
