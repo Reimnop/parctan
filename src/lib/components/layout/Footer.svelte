ffda4c7eb36f03ceed16ab28751827cbbcba7b46
enchart <77974051+enchart@users.noreply.github.com>
2023-08-17 19:56:57 +0500

    Updated footer

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -19,7 +19,7 @@
   ];
 </script>
 
-<footer class="flex flex-col gap-y-2 pt-4 md:flex-row md:items-center md:justify-between">
+<footer class="flex flex-col gap-y-2 pt-4 mt-4 md:flex-row md:items-center md:justify-between">
   <div class="flex items-center gap-x-4">
     <img class="row-span-2 h-5 max-w-none" src="/assets/images/icon.svg" alt="Parctan icon" />
     <div class="divider divider-horizontal mx-0 hidden w-1 md:flex" />
